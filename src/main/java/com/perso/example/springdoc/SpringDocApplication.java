8c29e2fa542cfaa9fa74ffa37937fac9874802fc
abenjell <33379974+abenjell@users.noreply.github.com>
2022-06-16 15:13:03 +0200

    Update SpringDocApplication.java
    add filters

--- src/main/java/com/perso/example/springdoc/SpringDocApplication.java
+++ src/main/java/com/perso/example/springdoc/SpringDocApplication.java
@@ -3,6 +3,7 @@ package com.perso.example.springdoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
@@ -10,6 +11,17 @@ public class SpringDocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDocApplication.class, args);
+    }
+
+
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setIncludeQueryString(true);
+        return loggingFilter;
     }
 
     @Bean
